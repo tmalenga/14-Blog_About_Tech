053a6bea0708e9affeeb1b12203655ba01f8a427
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-22 20:41:19 -0300

    update users.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 const {Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
 
 User.init({
     id:{
@@ -43,7 +48,24 @@ User.init({
     freezeTableName: true,
     underscored: true,
     modelName: 'user'
-  }
+  },
+  {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+    }
 );
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 app.listen(PORT, () => console.log('Now listening'));
 
