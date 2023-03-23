3821b1ad1d416d8a4accec7bf56a6bd72310dc1f
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-22 21:02:38 -0300

    Update file structure

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-const path = require('path');
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const session = require('express-session');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
 
-app.listen(PORT, () => console.log('Now listening'));
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
 
