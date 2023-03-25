085bef32f85200926c3c28c1ef785ce10ff2f562
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-25 13:05:32 -0300

    Update code

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//const routes = require('./controllers');
 const session = require('express-session');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -22,7 +23,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+//app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
