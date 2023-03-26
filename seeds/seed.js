6ae203af4d36551f67219fcd592dd11f93b1378c
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-26 18:49:17 -0300

    Update seeds file

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const postData = require('./postData.json');
+const userData = require('./userData.json');
+const commentsData = require('./commentsData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+    });
+  }
+
+  for (const comments of commentsData) {
+    await Comment.create({
+      ...comments,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
+
+module.exports = seedDatabase;
